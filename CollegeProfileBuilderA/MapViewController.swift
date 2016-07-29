9df92de4d716a4acfeb231e9e3786c84ce187154
Saurav Desai <esowmya@sauravs-macbook-pro-2.local>
2016-07-29 13:00:02 -0500

    final college profile builder

--- CollegeProfileBuilderA/MapViewController.swift
+++ CollegeProfileBuilderA/MapViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 import MapKit
 
 class MapViewController: UIViewController, UITextFieldDelegate {
-
+    
     @IBOutlet weak var mapView: MKMapView!
     @IBOutlet weak var textField: UITextField!
     var university : College!
